e935f0c58ca15b2d48bccbfdf3602c57f1ce2545
kinntaro <moneybook0409@outlook.jp>
2019-08-17 13:12:57 +0900

    js ver.1です

---
+++ timetable.js
@@ -0,0 +1,50 @@
+
+var button=document.querySelectorAll('[id^=star');//idがstarであるものをget
+var num;
+var str=[];
+var table=[];
+var check=[];
+arrayList();
+
+function arrayList(){
+var table=document.querySelectorAll("[id^=fes");
+    
+    for(var i=0;i<table.length;i++){
+         console.log(table[i-1]);
+        
+         
+         str[i]=table[i].innerHTML;
+         check[i]=0;
+        
+        }
+    }
+
+
+button.forEach((button)=>{
+button.addEventListener("click",function(){
+
+    console.log(button);
+    var eventId=event.target.id;
+
+    if(check[eventId[4]]==0){
+    check[eventId[4]]=1;
+    sort(eventId[4]);
+    }
+
+    else{
+        check[eventId[4]]=0;
+    }
+
+    
+});
+});
+
+
+function sort(num){
+
+ 
+    
+ document.querySelector('#point').insertAdjacentHTML('afterbegin',str[num]);
+
+} 
+
