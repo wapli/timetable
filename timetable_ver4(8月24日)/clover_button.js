d851e8c93da3d19a6d6867a1e33a4d5ac851fbe8
shouta0702 <shota90340@yahoo.co.jp>
2019-08-24 12:13:39 +0900

    各イベントを枠線によって浮き上がらせてボタンぽくするつもりでしたが、セルの大きさによっては不自然になってしまうので代わりに影をつけてみました。 また各イベントのレイアウトを作りましたが、画像の大きさを変更するのができなかってので、プログラミング以前に画像の大きさを変更するなどの修正が必要です。

---
+++ timetable_ver4(8月24日)/clover_button.js
@@ -0,0 +1,35 @@
+var star=document.getElementsByClassName('star');
+var favorite=document.getElementsByClassName('favorite');
+var count=[];
+for (let j = 0; j<favorite.length; j++) {
+    count[j] = 0;
+  }
+  
+
+function changesytle(cssid,cssfile) {
+    document.getElementById(cssid).href = cssfile;
+ }//色を変えるための関数です
+ for(let i=0;i<favorite.length;i++){
+    favorite[i].addEventListener('click', function(e) {//クラスでボタンを判断
+       console.log(star[i]);
+        console.log(i);
+        favorite_change(star[i],i);
+
+
+})}
+
+
+ 
+function favorite_change(star,i){
+    console.log(count[i]);
+    console.log(star);
+    if(count[i]===0){            
+        star.style.color='#ff0000';
+        count[i]=1;
+    }
+    else{
+        star.style.color='#FFFFFF';
+        count[i]=0;
+    }
+}
+
